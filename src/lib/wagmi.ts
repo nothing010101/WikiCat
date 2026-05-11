f73e9a89b24dd8861e4e0c481eb56995dbc05c76
nothing010101 <mstrj719@gmail.com>
2026-05-11 20:32:26 +0700

    fix: set Reown projectId for wallet connection

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -4,7 +4,7 @@ import { base } from "wagmi/chains";
 export const config = getDefaultConfig({
   appName: "Wikicat ($WIKI)",
   projectId:
-    process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "wikicat_default",
+    process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "18b4abeb470c796e1c8a1dc8bff56195",
   chains: [base],
   ssr: true,
 });
